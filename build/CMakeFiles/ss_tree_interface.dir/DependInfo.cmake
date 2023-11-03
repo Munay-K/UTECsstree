73f88175e544a16716a53e68884910dc79ba4935
munay-k <gonzalo.alfacaso@gmail.com>
2023-11-02 21:44:04 -0500

    done

---
+++ build/CMakeFiles/ss_tree_interface.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/munay/University/2023-02/CS3014_EDA/labs/UTECsstree/tinyfiledialogs.c" "CMakeFiles/ss_tree_interface.dir/tinyfiledialogs.c.o" "gcc" "CMakeFiles/ss_tree_interface.dir/tinyfiledialogs.c.o.d"
+  "/home/munay/University/2023-02/CS3014_EDA/labs/UTECsstree/CortexAPI.cpp" "CMakeFiles/ss_tree_interface.dir/CortexAPI.cpp.o" "gcc" "CMakeFiles/ss_tree_interface.dir/CortexAPI.cpp.o.d"
+  "/home/munay/University/2023-02/CS3014_EDA/labs/UTECsstree/Interface.cpp" "CMakeFiles/ss_tree_interface.dir/Interface.cpp.o" "gcc" "CMakeFiles/ss_tree_interface.dir/Interface.cpp.o.d"
+  "/home/munay/University/2023-02/CS3014_EDA/labs/UTECsstree/SStree.cpp" "CMakeFiles/ss_tree_interface.dir/SStree.cpp.o" "gcc" "CMakeFiles/ss_tree_interface.dir/SStree.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ss_tree_test.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/munay/University/2023-02/CS3014_EDA/labs/UTECsstree/SStree.cpp" "CMakeFiles/ss_tree_test.dir/SStree.cpp.o" "gcc" "CMakeFiles/ss_tree_test.dir/SStree.cpp.o.d"
+  "/home/munay/University/2023-02/CS3014_EDA/labs/UTECsstree/main.cpp" "CMakeFiles/ss_tree_test.dir/main.cpp.o" "gcc" "CMakeFiles/ss_tree_test.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
